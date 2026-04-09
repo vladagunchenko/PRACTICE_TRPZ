820a0cb6cf0075472598eb7be697883c73d4346e
Raakkaani <lirodaki345@gmail.com>
2026-04-09 16:12:03 +0300

    нікнейми

--- src/js/editor/firebaseauth.js
+++ src/js/editor/firebaseauth.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVhJoj2QCmOlWAc9EgvcFFldD8yLp1l64",
@@ -14,6 +15,48 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+document.getElementById('submit-r').addEventListener('click', async (e)=>{
+  e.preventDefault();
+  const username = document.getElementById('user-register').value;
+  const email_r = document.getElementById('email-register').value;
+  const password_r = document.getElementById('password-register').value;
+  if (username === '') {
+        alert("Enter a username");
+        return;
+  }
+  const auth = getAuth();
+  const db = getFirestore();
+
+  createUserWithEmailAndPassword(auth, email_r, password_r)
+.then(async (userCredential) => {
+    const user = userCredential.user;
+    const userData = {
+        email: email_r,
+        username: username
+    };
+    const docRef = doc(db, "users", user.uid);
+    await setDoc(docRef, userData);
+    window.location.href = "home.html";
+})
+.catch((error) => {
+    alert(error.message);
+});
+})
+
+const signIn = document.getElementById('submit-l');
+signIn.addEventListener('click', (event)=>{
+  event.preventDefault();
+  const email_l = document.getElementById('login-email').value;
+  const password_l = document.getElementById('login-password').value;
+  const auth = getAuth();
+   signInWithEmailAndPassword(auth, email_l, password_l)
+  .then(async (userCredential) => {
+    const user = userCredential.user;
+    window.location.href = "home.html";
+  })
+})
+/*const auth = getAuth(app);
   const submit_r = document.getElementById('submit-r');
   const submit_l = document.getElementById('submit-l');
 submit_r.addEventListener("click", function(event){
@@ -56,4 +99,4 @@ submit_l.addEventListener("click", function(event){
     alert(errorMessage)
   });
 
-});
+});*/
---
+++ src/js/editor/homepage.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVhJoj2QCmOlWAc9EgvcFFldD8yLp1l64",
+  authDomain: "login-form-ee124.firebaseapp.com",
+  projectId: "login-form-ee124",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+
+      document.querySelectorAll('.name-user').forEach(el => {
+        el.innerText = data.username;
+      });
+    }
+  }
+});
